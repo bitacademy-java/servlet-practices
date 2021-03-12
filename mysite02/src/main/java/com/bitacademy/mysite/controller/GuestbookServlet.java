1cc6c99b344892e77f28b179444aeaf3df819472
bit-python-fund <peterpmk@naver.com>
2021-03-12 15:43:25 +0900

    mysite02 JSTL&EL 작업

--- mysite02/src/main/java/com/bitacademy/mysite/controller/GuestbookServlet.java
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/GuestbookServlet.java
@@ -1,11 +1,15 @@
 package com.bitacademy.mysite.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bitacademy.mysite.dao.GuestbookDao;
+import com.bitacademy.mysite.vo.GuestbookVo;
 import com.bitacademy.web.mvc.WebUtil;
 
 public class GuestbookServlet extends HttpServlet {
@@ -19,6 +23,8 @@ public class GuestbookServlet extends HttpServlet {
 		if("deleteform".equals(action)) {
 			
 		} else {
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+			request.setAttribute("list", list);
 			WebUtil.forward("/WEB-INF/views/guestbook/index.jsp", request, response);
 		}
 	}
